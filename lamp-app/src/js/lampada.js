9f0c998bed6169cbe2602f6c093dbf45d12fa83b
Gabriel Fernandes <115189317+GabrielFernandes251@users.noreply.github.com>
2022-10-29 12:25:28 -0300

    added local files

---
+++ lamp-app/src/js/lampada.js
@@ -0,0 +1,8 @@
+
+function showImageLampada() {
+    document.getElementById('imglampada').src = 'src/img/lampada-yellow.png'
+}
+
+function CloseImageLampada() {
+    document.getElementById('imglampada').src = 'src/img/lampada-de-ideia.png'
+}
